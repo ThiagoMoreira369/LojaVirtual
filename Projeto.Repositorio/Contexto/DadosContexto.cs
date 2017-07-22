ea4f6d95f9a00d5067a48b30f7412b4d02721866
Thiago Moreira <tmoreiradossantos@yahoo.com.br>
2017-07-22 20:48:47 -0300

    teste

---
+++ Projeto.Repositorio/Contexto/DadosContexto.cs
@@ -0,0 +1,26 @@
+﻿using Projeto.Entidades;
+using Projeto.Repositorio.Mapeamento;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Projeto.Repositorio.Contexto
+{
+    public class DadosContexto : DbContext
+    {
+        public DadosContexto()
+            : base(ConfigurationManager.
+                 ConnectionStrings["loja"].ConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Configurations.Add(new MapeamentoProduto());
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
