2c825ff7471aada1fc75961dc5f280da621eaa27
Thiago Moreira <tmoreiradossantos@yahoo.com.br>
2017-07-22 21:25:50 -0300

    Loja Virtual
    Alterando repositório.

---
+++ Projeto.Web/Models/ItemCesta.cs
@@ -0,0 +1,18 @@
+﻿using Projeto.Entidades;
+
+namespace Projeto.Web.Models
+{
+    public class ItemCesta
+    {
+        public int Quantidade { get; set; }
+        public Produto Produto { get; set; }
+
+        public decimal ValorTotal
+        {
+            get
+            {
+                return Produto.Preco * Quantidade;
+            }
+        }
+    }
+}
